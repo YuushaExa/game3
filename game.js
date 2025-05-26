eab0f9a5f5e06869cf9fa1a88c9a6e8329d91dbe
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-05-27 02:13:52 +0300

    Update game.js

--- game.js
+++ game.js
@@ -74,9 +74,7 @@ const gameData = {
                 {
                     name: "Character 2",
                     image: "avatar2.jpg",
-                    text: "This is the third dialogue.", 
-        }
-            ],
+                    text: "This is the third dialogue."
  choices: [
                 {
                     text: "Go to the park",
@@ -90,6 +88,8 @@ const gameData = {
                     text: "Ask for more information",
                     next_scene: "info_scene"
                 }
+            ]
+        }
             ],
             scene: {
                 time: "25", // in seconds
